fc6152131a143ffc5eb98745d3be9ea59943bc87
predeep kumar singroul <prasingroul4899@gmail.com>
2023-01-26 17:59:28 +0530

    Simple controller

--- _12_SpringBoot_Project---1/src/main/java/com/springBoot/Controller/StudentController.java
+++ _12_SpringBoot_Project---1/src/main/java/com/springBoot/Controller/StudentController.java
@@ -1,14 +1,41 @@
 package com.springBoot.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springBoot.Model.Student;
+
 @RestController
+@RequestMapping("/hellos")
 public class StudentController {
 
 	@GetMapping("/sayHello")
 	public String sayHello() {
 		return "welcome sir";
+	}
+	
+	
+	@GetMapping("/student")
+	public Student student() {
+		return new Student("pradeep", 125, 960);
+	}
+	
+	@GetMapping("/getStudent/{roll}")
+	public Student students(@PathVariable("roll") Integer roll) {
+		return new Student("gfhg", roll, 960);
+	}
+	
+	@GetMapping("/getStudent/{roll}/{name}")
+	public Student studentsss(@PathVariable("roll") Integer roll,@PathVariable("name") String name) {
+		return new Student(name, roll, 960);
+	}
+	
+	@GetMapping("/getStudent")
+	public Student studentss(@RequestParam Integer roll,@RequestParam String name) {
+		return new Student(name, roll, 960);
 	}
 	
 }
