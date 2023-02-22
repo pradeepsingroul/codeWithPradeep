05dd794aacf00a8425e389fc1b4a81e8eb5d4f41
predeep kumar singroul <prasingroul4899@gmail.com>
2023-02-22 22:38:32 +0530

    springBoot_1

---
+++ _13_SpringBoot_Project---2/src/main/java/com/springBoot/Model/Main.java
@@ -0,0 +1,12 @@
+package com.springBoot.Model;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Student s = new Student();
+	
+		
+	}
+	
+}
---
+++ _13_SpringBoot_Project---2/src/main/java/com/springBoot/Model/Student.java
@@ -0,0 +1,12 @@
+package com.springBoot.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Student {
+
+	private int roll;
+	private String name;
+}
