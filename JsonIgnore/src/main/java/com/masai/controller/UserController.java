7a152272f858844a4f96b3e9c38c6b7ef543a84d
predeep kumar singroul <prasingroul4899@gmail.com>
2023-02-22 22:30:35 +0530

    json_ingore

--- JsonIgnore/src/main/java/com/masai/controller/UserController.java
+++ JsonIgnore/src/main/java/com/masai/controller/UserController.java
@@ -2,17 +2,17 @@ package com.masai.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.masai.Services.AddressServices;
 import com.masai.Services.UserService;
 import com.masai.model.Address;
 import com.masai.model.User;
+
+
 
 @RestController
 @RequestMapping("/user")
